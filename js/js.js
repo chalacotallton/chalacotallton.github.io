8ef3270f17301d3d20bcc7cbe3db24ef84b20561
T C Lacerda Santos <talltonchalaco@gmail.com>
2019-10-24 19:12:45 -0300

    moved 1 function to js.js

--- js/js.js
+++ js/js.js
@@ -1,3 +1,10 @@
 function mobilenavbar() {
   $("#mobilenavbar").show();
 }
+function outsideclick() {
+  $(document).click(function(e) {
+    if(e.target.id != "mobilenavbar" && e.target.id != "ddmenu") {
+      $("#mobilenavbar").hide(600);
+    }
+  })
+}
