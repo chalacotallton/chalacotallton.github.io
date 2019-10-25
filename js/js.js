e7bfa6e0476b7b856a35ce2a2ffc22a6f5066022
T C Lacerda Santos <talltonchalaco@gmail.com>
2019-10-25 19:24:09 -0300

    index layout finished

--- js/js.js
+++ js/js.js
@@ -1,10 +1,11 @@
-function mobilenavbar() {
-  $("#mobilenavbar").show();
-}
-function outsideclick() {
   $(document).click(function(e) {
-    if(e.target.id != "mobilenavbar" && e.target.id != "ddmenu") {
-      $("#mobilenavbar").hide(600);
+    if(e.target.id != "mobilenavbarContent" && e.target.id != "ddmenu") {
+      $("#mobilenavbar").hide();
+    }
+    if(e.target.id == "ddmenu") {
+      $("#mobilenavbar").show();
     }
   })
-}
+$(document).ready(function(){
+  console.log("ready");
+})
