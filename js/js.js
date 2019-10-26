fd5c89f2bbdf124da0a4a7964a202091f348e7c4
T C Lacerda Santos <talltonchalaco@gmail.com>
2019-10-26 03:42:05 -0300

    finally finished navbar

--- js/js.js
+++ js/js.js
@@ -1,9 +1,13 @@
   $(document).click(function(e) {
     if(e.target.id != "mobilenavbarContent" && e.target.id != "ddmenu") {
       $("#mobilenavbar").hide();
+      $("#ddmenu").text("|||");
+      $("#ddmenuClose").hide();
     }
     if(e.target.id == "ddmenu") {
       $("#mobilenavbar").show();
+       $("#ddmenu").html("&nbsp");
+      $("#ddmenuClose").show();
     }
   })
 $(document).ready(function(){
