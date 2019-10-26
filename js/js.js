3572b24a7fec8805a6c774cbb559ea1605abc4be
T C Lacerda Santos <talltonchalaco@gmail.com>
2019-10-26 19:13:00 -0300

    asd

--- js/js.js
+++ js/js.js
@@ -1,9 +1,11 @@
   $(document).click(function(e) {
-    if(e.target.id === "undefined") {
+    if(e.target.id === "") {
         $("#mobilenavbar").hide();
         $("#ddmenu").text("|||");
         $("#ddmenuClose").hide();
+        console.log(e.target.id);
     }
+    console.log(e.target.id);
     // if(e.target.id != "mobilenavbarContent" && e.target.id != "ddmenu") {
     //   $("#mobilenavbar").hide();
     //   $("#ddmenu").text("|||");
