c97613406e17ad7af25f534dc8992e3ad14db60f
T C Lacerda Santos <talltonchalaco@gmail.com>
2019-10-26 19:08:06 -0300

    js change

--- js/js.js
+++ js/js.js
@@ -1,9 +1,15 @@
   $(document).click(function(e) {
-    if(e.target.id != "mobilenavbarContent" && e.target.id != "ddmenu") {
-      $("#mobilenavbar").hide();
-      $("#ddmenu").text("|||");
-      $("#ddmenuClose").hide();
+    if(e.target.id === "undefined") {
+        $("#mobilenavbar").hide();
+        $("#ddmenu").text("|||");
+        $("#ddmenuClose").hide();
     }
+    // if(e.target.id != "mobilenavbarContent" && e.target.id != "ddmenu") {
+    //   $("#mobilenavbar").hide();
+    //   $("#ddmenu").text("|||");
+    //   $("#ddmenuClose").hide();
+    //   console.log(e.target.id);
+    // }
     if(e.target.id == "ddmenu") {
       $("#mobilenavbar").show();
        $("#ddmenu").html("&nbsp");
