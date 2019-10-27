826ee37f66396cf9fdbe48bdccdabf5aa5b89166
T C Lacerda Santos <talltonchalaco@gmail.com>
2019-10-27 01:03:34 -0300

    fixed a bug affecting navbar on iphone

--- js/js.js
+++ js/js.js
@@ -3,18 +3,23 @@ $(document).ready(function(){
   $("#logo").css("left", logo_width);
   console.log("ok");
 })
-  $(document).click(function(e) {
-    if(e.target.id != "mobilenavbarContent" && e.target.id != "ddmenu") {
-      $("#mobilenavbar").hide();
-      $("#ddmenu").text("|||");
-      $("#ddmenuClose").hide();
-    }
-    if(e.target.id == "ddmenu") {
-      $("#mobilenavbar").show();
-       // $("#ddmenu").html("&nbsp");
-      $("#ddmenuClose").show();
-    }
-  })
+$(window).resize(function(){
+  var logo_width = $("#navbar").width()/2-$("#logo").width()/2;
+  $("#logo").css("left", logo_width);
+  console.log("ok");
+})
+
+$(document).click(function(e) {
+  if(e.target.id != "mobilenavbarContent" && e.target.id != "ddmenu") {
+    $("#mobilenavbar").hide();
+    $("#ddmenu").text("|||");
+    $("#ddmenuClose").hide();
+  }
+  if(e.target.id == "ddmenu") {
+    $("#mobilenavbar").show();
+    $("#ddmenuClose").show();
+  }
+})
 $(document).ready(function(){
   console.log("ready");
 })
