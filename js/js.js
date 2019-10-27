e7a3e995aa9b21e8040b7ade611ff57e364bd7a7
T C Lacerda Santos <talltonchalaco@gmail.com>
2019-10-27 03:22:55 -0300

    reworked navbar, now its ok.

--- js/js.js
+++ js/js.js
@@ -1,24 +1,15 @@
-/*navbar*/
-$(document).ready(function(){
-  var logo_width = $("#navbar").width()/2-$("#logo").width()/2;
-  $("#logo").css("left", logo_width);
+$(document).ready(function() {
+  $(document).click(function(e) {
+    if(e.target.id == "ddmenu") {
+      if($("#ddmenu").text() == "X") {
+        $("#ddmenu").text("|||");
+      }
+      else {
+        $("#ddmenu").text("X");
+      }
+    }
 })
-$(window).resize(function(){
-  var logo_width = $("#navbar").width()/2-$("#logo").width()/2;
-  $("#logo").css("left", logo_width);
 })
-$(document).click(function(e) {
-  if(e.target.id != "mobilenavbarContent" && e.target.id != "ddmenu") {
-    $("#mobilenavbar").hide();
-    $("#ddmenuClose").hide();
-    console.log("ok");
-  }
-  if(e.target.id == "ddmenu") {
-    $("#mobilenavbar").show();
-    $("#ddmenuClose").show();
-  }
-})
-/*-----*/
 
 $(document).ready(function(){
   console.log("ready");
