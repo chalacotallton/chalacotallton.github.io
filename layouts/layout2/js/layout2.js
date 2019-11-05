cf29bd0c43ad34adc7758f0bf990e8a287727a4d
T C Lacerda Santos <talltonchalaco@gmail.com>
2019-11-05 00:25:34 -0300

    added a showcase to layout2

--- layouts/layout2/js/layout2.js
+++ layouts/layout2/js/layout2.js
@@ -22,7 +22,38 @@ window.onscroll = function() {
 
 //vitrine
 $(document).ready(function(){
-  console.log($("#vitrine").width());
-  var d = document.getElementById("vitrine");
-  d.style.left = "-300px";
+  var x = document.getElementsByClassName("vitrine-item");
+  var vitrineItemLength = 0;
+  for (var i = 0 ; i < x.length; i++) {
+    vitrineItemLength += $(x[i]).outerWidth();
+  }
+  if (vitrineItemLength > $(".vitrine").outerWidth()) {
+    console.log("flag on");
+  }
+  else {
+    console.log($(".vitrine").outerWidth()+" flag off");
+  }
+
+});
+//vitrine controls
+
+// $(document).ready(function(){
+//   var x = document.getElementsByClassName("vitrine-item");
+//   var distance = 0;
+//   for(var i = 1; i < x.length; i++) {
+//     x[i].style.left = -$(x[i]).outerWidth() - parseInt($(x[i]).css('marginRight')) + "px";
+//     distance += -$(x[i]).outerWidth() - parseInt($(x[i]).css('marginRight'));
+//   }
+//   x[0].style.left = -distance + "px";
+// })
+
+// on resize recalculate x[0] position
+$(document).ready(function(){
+    $(window).resize(function() {
+      console.log("resized");
+      var x = document.getElementsByClassName("vitrine-item");
+      for(var i = 0; i < x.length; i++) {
+          $(x[i]).css("left", "");
+      }
+    })
 });
