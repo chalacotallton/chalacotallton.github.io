1f6552250d8ab0129f3cec31498e1894709d01e4
T C Lacerda Santos <talltonchalaco@gmail.com>
2019-11-14 16:29:40 -0300

    bug fix navbar template

--- Template/js/template.js
+++ Template/js/template.js
@@ -6,15 +6,23 @@ console.log($(window).width());
 $(document).ready(function() {
 $(document).click(function(e) {
 if(e.target.id == "ddmenu") {
-  if($("#ddmenu").text() == "X") {
-    $("#ddmenu").html("&equiv;");
-    $("#ddmenu").css("font-size", "28px");
-    $("#ddmenu").removeClass("py-2");
-  }
-  else {
+  if($("#ddmenu").text() !== "X" && !$("#navbarCollapse").hasClass("fechado")) {
     $("#ddmenu").css("font-size", "20px");
     $("#ddmenu").text("X");
     $("#ddmenu").addClass("py-2");
+    $("#navbarCollapse").addClass("aberto");
+    setTimeout(function() {
+      $("#navbarCollapse").removeClass("aberto");
+    }, 351)
+  }
+  else if(!$("#navbarCollapse").hasClass("aberto")){
+    $("#ddmenu").html("&equiv;");
+    $("#ddmenu").css("font-size", "28px");
+    $("#ddmenu").removeClass("py-2");
+    $("#navbarCollapse").addClass("fechado");
+    setTimeout(function() {
+      $("#navbarCollapse").removeClass("fechado");
+    }, 351)
   }
 }
 })
